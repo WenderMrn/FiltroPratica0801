2d0ba716eb500fa4cb5794d163ba5c2aa4d15e68
WenderMrn <wender.mrn@gmail.com>
2017-01-31 22:53:35 -0300

    criado filtro para o servlet A e servlete b

---
+++ src/main/java/br/edu/ifpb/filter/FiltroA.java
@@ -0,0 +1,35 @@
+package br.edu.ifpb.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(urlPatterns={"/a.do"})
+public class FiltroA implements Filter{
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		System.out.println("FiltroA pre-processamento...");
+		chain.doFilter(request, response);
+		System. out.println("FiltroA pos-processamento...");
+		
+	}
+
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
